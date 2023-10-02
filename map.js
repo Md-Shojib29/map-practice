0dea8a7a1bd0aea07f445d97bc6aa5029b26e046
Md-Shojib29 <info.username420@gmail.com>
2023-10-02 19:37:29 +0600

    map commit

---
+++ map.js
@@ -0,0 +1,10 @@
+let map = new map();
+map.set('1', 'str1');  
+map.set(1, 'num1');    
+map.set(true, 'bool1'); 
+alert( map.get(1)   );
+
+alert( map.get(1)   );
+alert( map.get('1') );
+
+alert( map.size );
